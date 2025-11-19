7b7ce775351c7de173144e81d05375ec7d205aa0
YudGnas <teemokhongngu@gmail.com>
2025-11-19 16:01:33 +0700

    first

---
+++ ScreenClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+class ScreenClient
+{
+    [DllImport("user32.dll")]
+    static extern int GetSystemMetrics(int nIndex);
+    const int SM_CXSCREEN = 0;
+    const int SM_CYSCREEN = 1;
+
+    static void Main()
+    {
+        string serverIP = "127.0.0.1"; // đổi IP server
+        int port = 5000;
+
+        TcpClient client = new TcpClient();
+        client.Connect(serverIP, port);
+        NetworkStream ns = client.GetStream();
+        Console.WriteLine("Connected to server");
+
+        int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+        while (true)
+        {
+            Bitmap bmp = new Bitmap(screenWidth, screenHeight);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+            }
+
+            byte[] data;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var encoder = ImageCodecInfo.GetImageEncoders()
+                             .First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+                var encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, 50L);
+                bmp.Save(ms, encoder, encoderParams);
+                data = ms.ToArray();
+            }
+
+            // gửi kích thước ảnh trước
+            byte[] lengthBytes = BitConverter.GetBytes(data.Length);
+            ns.Write(lengthBytes, 0, 4);
+
+            // gửi ảnh
+            ns.Write(data, 0, data.Length);
+
+            Thread.Sleep(33); // ~30FPS
+        }
+    }
+}
---
+++ ScreenStreaming/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ScreenStreaming
+{
+    public partial class MainWindow : Window
+    {
+        private const int PORT = 5000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            Task.Run(async () =>
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, PORT);
+                listener.Start();
+                Console.WriteLine($"Server listening on port {PORT}");
+
+                while (true)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    Console.WriteLine("Client connected");
+                    _ = HandleClient(client);
+                }
+            });
+        }
+
+        private async Task HandleClient(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] lengthBuffer = new byte[4];
+
+            while (true)
+            {
+                try
+                {
+                    int read = await ns.ReadAsync(lengthBuffer, 0, 4);
+                    if (read == 0) break;
+                    int imgLength = BitConverter.ToInt32(lengthBuffer, 0);
+
+                    byte[] imgBuffer = new byte[imgLength];
+                    int offset = 0;
+                    while (offset < imgLength)
+                    {
+                        int bytesRead = await ns.ReadAsync(imgBuffer, offset, imgLength - offset);
+                        if (bytesRead == 0) break;
+                        offset += bytesRead;
+                    }
+
+                    BitmapImage img = new BitmapImage();
+                    img.BeginInit();
+                    img.StreamSource = new MemoryStream(imgBuffer);
+                    img.CacheOption = BitmapCacheOption.OnLoad;
+                    img.EndInit();
+                    img.Freeze();
+
+                    Dispatcher.Invoke(() => imgView.Source = img);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            client.Close();
+            Console.WriteLine("Client disconnected");
+        }
+    }
+}
